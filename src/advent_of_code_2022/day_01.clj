0f9e869d25bb958f90fba538242fde1839862461
Riccardo Valentini <riccardovalentini1854@gmail.com>
2022-12-01 19:54:08 +0100

    Day 01 - complete

--- src/advent_of_code_2022/day_01.clj
+++ src/advent_of_code_2022/day_01.clj
@@ -1,12 +1,15 @@
-(ns advent-of-code-2022.day_01)
+(ns advent-of-code-2022.day_01
+  (:require [clojure.java.io :as io]))
 
-(defn exec
-  "Invoke me with clojure -X advent-of-code-2022.advent-of-code-2022/exec"
-  [opts]
-  (println "exec with" opts))
+(comment
+  (with-open [rdr (io/reader (io/resource "day_01_input.txt"))]
+    (let [calories (->> (line-seq rdr)
+                  (partition-by #(= % ""))
+                  (take-nth 2)
+                  (map #(map parse-long %))
+                  (map #(reduce + %)))]
+      (println "Top elf: " (apply max calories))
+      (println "Top three elves: " (apply + (take 3 (reverse (sort calories)))))))
 
-(defn -main
-  "Invoke me with clojure -M -m advent-of-code-2022.advent-of-code-2022"
-  [& args]
-  (println "-main with" args))
+  )
 
