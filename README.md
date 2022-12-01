f339c79c368317bea271f6018539460d34278855
Riccardo Valentini <riccardovalentini1854@gmail.com>
2022-12-01 12:40:42 +0100

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# advent-of-code-2022
+
+Solutions for the Advent of Code Challenge 2022 in Clojure!
+
---
+++ src/advent_of_code_2022/day_01.clj
@@ -0,0 +1,12 @@
+(ns advent-of-code-2022.day_01)
+
+(defn exec
+  "Invoke me with clojure -X advent-of-code-2022.advent-of-code-2022/exec"
+  [opts]
+  (println "exec with" opts))
+
+(defn -main
+  "Invoke me with clojure -M -m advent-of-code-2022.advent-of-code-2022"
+  [& args]
+  (println "-main with" args))
+
