0a95365c607b1176dba99f09a93a7a9001317887
Riccardo Valentini <riccardovalentini1854@gmail.com>
2022-12-14 08:15:40 +0100

    Day 14 - complete

--- src/advent_of_code_2022/day_14.clj
+++ src/advent_of_code_2022/day_14.clj
@@ -4,11 +4,82 @@
 
 (def input (slurp "resources/day_14_input.txt"))
 
+(def sand-source [500 0])
+(def x-start 100)
+(def x-end 900)
+
+(defn range->single [[[sx sy] [ex ey]]]
+  (if (= sx ex)
+    (for [y (range (min sy ey) (inc (max sy ey)))] [sx y])
+    (for [x (range (min sx ex) (inc (max sx ex)))] [x sy])))
+
 (defn parse-input [input]
-  (s/split-lines input))
+  (->> input
+    (s/split-lines)
+    (map u/all-numbers)
+    (map #(partition 2 %))
+    (map #(partition 2 1 %))
+    (map #(map range->single %))
+    (map #(apply concat %))
+    (apply concat)))
 
-(comment
-  (def lines (parse-input input))
-  lines
+(defn add-bottom [rocks]
+  (concat rocks (let [max-y (apply max (map second rocks))]
+                  (for [x (range x-start x-end)]
+                    [x (+ 2 max-y)]))))
 
-  )
+(defn get-pos [cave-map [x y]]
+  (get-in cave-map [y (- x x-start)]))
+
+(defn move-sand-to [cave-map [old-x old-y] [new-x new-y]]
+  (-> cave-map
+    (u/mark-at-pos "." [(- old-x x-start) old-y])
+    (u/mark-at-pos "o" [(- new-x x-start) new-y])))
+
+(defn blocked? [cave-map pos]
+  (not= (get-pos cave-map pos) "."))
+
+(defn move-sand [cave-map]
+  (loop [[x y :as pos] sand-source
+         cave-map (u/mark-at-pos cave-map "o" [(- x x-start) y])]
+    (let [down [x (inc y)]
+          down-left [(dec x) (inc y)]
+          down-right [(inc x) (inc y)]]
+      (if (not (blocked? cave-map down))
+        (recur down (move-sand-to cave-map pos down))
+        (if (not (blocked? cave-map down-left))
+          (recur down-left (move-sand-to cave-map pos down-left))
+          (if (not (blocked? cave-map down-right))
+            (recur down-right (move-sand-to cave-map pos down-right))
+            cave-map))))))
+
+(defn into-abyss? [cave-map]
+  (some #{"o"} (get cave-map 179)))
+
+(defn source-blocked? [cave-map]
+  (= "o" (get-pos cave-map [500 0])))
+
+(defn part1 [cave-map]
+  (->> cave-map
+    (iterate move-sand)
+    (take-while #(not (into-abyss? %)))
+    count
+    dec))
+
+(defn part2 [cave-map]
+  (->> cave-map
+    (iterate move-sand)
+    (take-while #(not (source-blocked? %)))
+    count))
+
+(let [rocks (add-bottom (parse-input input))
+      matrix (u/build-matrix {:x-dim (- x-end x-start)
+                              :y-dim 190
+                              :init-fn (constantly ".")})
+      cave-map (reduce
+                 (fn [m [x y]] (u/mark-at-pos m "#" [(- x x-start) y]))
+                 matrix
+                 rocks)]
+  (assert (= (part1 cave-map) 913))
+  (assert (= (part2 cave-map) 30762)))
+
--- src/advent_of_code_2022/util.clj
+++ src/advent_of_code_2022/util.clj
@@ -6,32 +6,66 @@
 (defn all-numbers [str]
   (mapv parse-long (re-seq #"-?[0-9]+" str)))
 
-(assert (= (all-numbers "blah -333") [-333]))
-(assert (= (all-numbers "blah -8 5 0 something -22 else  111") [-8 5 0 -22 111]))
-(assert (= (all-numbers "blah -10") [-10]))
-(assert (= (all-numbers "-1u0m22-1") [-1 0 22 -1]))
-
 (defn first-index-of [p xs]
   (->> xs
     (keep-indexed (fn [i x] (when (p x) i)))
     first))
 
+(defn first-repeated [[x & xs]]
+  (if (= x (first xs))
+    x
+    (recur xs)))
 
-(defn mark-with-at-pos [matrix i [x y]]
+(defn neighbors [[x y]]
+  (list [(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]))
+
+(defn neighbors-with-corners [[x y :as pos]]
+  (filter #(not= % pos)
+    (for [dx [0 -1 1]
+          dy [0 -1 1]]
+      [(+ x dx) (+ y dy)])))
+
+(defn build-matrix [{:keys [x-dim y-dim init-fn]}]
+  (vec (map vec (for [y (range 0 y-dim)]
+                  (for [x (range 0 x-dim)]
+                    (init-fn x y))))))
+
+(defn mark-at-pos [matrix i [x y]]
   (assoc-in matrix [y x] i))
 
-(defn print-rope [rope]
-  (let [matrix (vec (map vec (for [x (range 0 21)]
-                               (for [y (range 0 26)]
-                                 "."))))
-        head (first rope)
-        tail (peek rope)
-        middle (map-indexed (fn [i p] ["#" p]) (butlast (rest rope)))]
-    (doseq [l (reduce
-                (fn [matrix [i pos]] (mark-with-at-pos matrix i pos))
-                (-> matrix
-                  (mark-with-at-pos "H" head)
-                  (mark-with-at-pos "T" tail))
-                middle)]
-      (println l))
-    ))
+(defn mark-row [matrix i row]
+  (map-indexed (fn [idx r] (if (= idx row) (vec (map (constantly i) r)) r)) matrix))
+
+(defn mark-column [matrix i col]
+  (map (fn [r] (map-indexed (fn [idx c] (if (= idx col) i c)) r)) matrix))
+
+
+;testy test
+(assert (= (all-numbers "blah -333") [-333]))
+(assert (= (all-numbers "blah -8 5 0 something -22 else  111") [-8 5 0 -22 111]))
+(assert (= (all-numbers "blah -10") [-10]))
+(assert (= (all-numbers "-1u0m22-1") [-1 0 22 -1]))
+
+(assert (= (count (flatten (build-matrix {:x-dim 20 :y-dim 5 :init-fn (constantly ".")}))) 100 ))
+;sum of first 100 numbers
+(assert (= (apply + (flatten (build-matrix {:x-dim 20 :y-dim 5 :init-fn (fn [x y] (+ (inc x) (* y 20)))})))
+          5050))
+
+(assert (= (-> (build-matrix {:x-dim 20 :y-dim 5 :init-fn (constantly ".")})
+             (mark-row "x" 3)
+             flatten
+             ((fn [s] (filter #(= % "x") s)))
+             count)
+          20))
+
+(assert (= (-> (build-matrix {:x-dim 20 :y-dim 5 :init-fn (constantly ".")})
+             (mark-column "x" 7)
+             flatten
+             ((fn [s] (filter #(= % "x") s)))
+             count)
+          5))
+
+(assert (= :d (first-repeated '(:a :b :c :d :d :e :f))))
+
+(assert (= '([1 0] [1 2] [0 1] [0 0] [0 2] [2 1] [2 0] [2 2])
+          (neighbors-with-corners [1 1])))
