38bb1a37aa67cdce329628ee258e44b1767262bf
Riccardo Valentini <riccardovalentini1854@gmail.com>
2023-02-02 09:58:59 +0100

    Day 16 - complete

--- src/advent_of_code_2022/day_16.clj
+++ src/advent_of_code_2022/day_16.clj
@@ -5,7 +5,7 @@
 
 (def input (slurp "resources/day_16_input.txt"))
 
-(defn all-chars [input]
+(defn line-chars [input]
   (->> input
     (s/split-lines)
     (map u/all-chars)))
@@ -14,9 +14,6 @@
   (->> input
     (s/split-lines)
     (map u/all-numbers)))
-
-
-(apply hash-map (interleave [:a :b :c] (repeat 1)))
 
 (defn tunnel-graph [lines]
   (->> lines
@@ -29,135 +26,87 @@
                      to (keys graph)]
                  {(str from to) (get (u/dijkstra graph from to) to)})))
 
-;(get shortest-paths (str "AA" (first nil)))
-;(get valves-with-flow nil)
-;valves-with-flow
-
-(defn max-release [permutation shortest-paths valves]
-  (let [init-walk (get shortest-paths (str "AA" (first permutation)))]
-    (loop [[p & ps :as all] permutation
+(defn max-release [path shortest-paths valves time]
+  (let [init-walk (get shortest-paths (str "AA" (first path)))]
+    (loop [[p & ps :as all] path
            move 0
-           time (+ init-walk 1)
+           time-passed (+ init-walk 1)
            release 0
            sum-release 0]
-      ;(println "--------------")
-      ;(println "minute" time)
-      ;(println p "& " ps)
-      ;(println "move" move)
-      ;(println "release" release)
-      ;(println "sum-release" sum-release)
-      (if (> time 30)
+      (if (> time-passed time)
         sum-release
-        (if (= 0 move)
+        (if (zero? move)
           (recur
             ps
             (or (get shortest-paths (str p (first ps))) 0)
-            (inc time)
+            (inc time-passed)
             (+ release (or (get valves p) 0))
             (+ sum-release release))
           (recur
             all
             (dec move)
-            (inc time)
+            (inc time-passed)
             release
             (+ sum-release release)))))))
 
-
-
-
-;(defn shortest-paths)
-(def lines (all-chars input))
-(def flows (flow-rates input))
-(def valves (map #(get % 1) lines))
-(def valves-with-flow (reduce into (filter some? (map (fn [v [f]] (when (> f 0) {v f})) valves flows))))
-(def graph (tunnel-graph lines))
-(def shortest-paths (find-shortest-paths graph))
-
-(Throwable->map *e)
-
-#_(def permutation (reverse (map first (sort-by second (map (fn [[v flow]]
-                                                              (if (not= v "AA")
-                                                                [v (/ flow (get shortest-paths (str "AA" v)))]
-                                                                [v 0])) valves-with-flow)))))
-
-
-valves-with-flow
-shortest-paths
-
-(defn get-candidates [visited valves-with-flow]
-  (filter (fn [[v _]] (not (get visited v))) valves-with-flow))
-
-;(get-candidates #{"AA" "CC" "BB" "DD"} valves-with-flow)
-;(into #{"AA" "VV"} ["EE"])
+(defn extend-path [branch valve shortest-paths]
+  {:path (conj (:path branch) valve)
+   :visited (into (:visited branch) [valve])
+   :minute (+
+             (:minute branch)
+             (get shortest-paths (str (peek (:path branch)) valve))
+             1)})
 
 (defn branch&bound [valves-with-flow shortest-paths]
-  (loop [[branch & bs] [{:path ["AA"]
-                         :visited #{"AA"}
-                         :minute 1}]
-         finished []
-         iter 0]
-    ;(println "loop with ")
-    ;(println "branch " branch)
-    ;(println "finished " finished)
-    ;(println "iter " iter)
-    (when (= (mod iter 100000) 0) (println "iteration" (/ iter 1000) "k"))
-    (if (nil? branch)
-      finished
-      (if (or (> (:minute branch) 30) (= (count (:path branch)) (inc (count valves-with-flow))))
-        (recur bs (conj finished (:path branch)) (inc iter))
-        (let [candidates (get-candidates (:visited branch) valves-with-flow)
-              new-paths (reduce (fn [paths [v _]]
-                                  (conj paths {:path (conj (:path branch) v)
-                                               :visited (into (:visited branch) [v])
-                                               :minute (+
-                                                         (:minute branch)
-                                                         (get shortest-paths (str (peek (:path branch)) v))
-                                                         1)}))
-                          []
-                          candidates)]
-          (recur (into bs new-paths) finished (inc iter)))))))
+  (let [time-limit-reached? #(> (:minute %) 26)
+        all-valves-open? #(= (count (:path %)) (inc (count valves-with-flow)))
+        get-candidates #(filter (fn [[v _]] (not (% v))) valves-with-flow)]
+    (loop [[branch & bs] [{:path ["AA"] :visited #{"AA"} :minute 1}]
+           finished []]
+      (if (nil? branch)
+        finished
+        (if (or (time-limit-reached? branch) (all-valves-open? branch))
+          (recur bs (conj finished (:path branch)))
+          (let [candidates (get-candidates (:visited branch))
+                new-paths (reduce
+                            (fn [paths [v _]] (conj paths (extend-path branch v shortest-paths)))
+                            []
+                            candidates)]
+            (recur (into bs new-paths) finished)))))))
 
+(defn valves->flow [chars numbers]
+  (->> [chars (flatten numbers)]
+    (apply map vector)
+    (filter (comp pos? second))
+    (into {})))
 
-;(peek ["AA" "BB" "CC"])
-;(conj ["AA" "BB" "CC"] "UU")
-(def branches (branch&bound valves-with-flow shortest-paths))
-branches
-(println "max release" (apply max (map #(max-release (rest %) shortest-paths valves-with-flow) branches)))
+(defn part1 [shortest-paths valves-with-flow]
+  (let [branches (branch&bound valves-with-flow shortest-paths)]
+    (->> branches
+      (map #(max-release (rest %) shortest-paths valves-with-flow 30))
+      (apply max))))
 
-;(max-release  [ "DD" "BB" "JJ" "HH" "EE" "CC"] shortest-paths valves-with-flow)
+(defn part2 [shortest-paths valves-with-flow]
+  (let [balanced-subsets (fn [xs] (every? #(> (count %) 6) xs))
+        branches-with-elephant (->> (combo/partitions valves-with-flow {:min 2 :max 2})
+                                 (filter balanced-subsets)
+                                 (map (fn [xs] (map #(branch&bound % shortest-paths) xs))))]
+    (->> branches-with-elephant
+      (map (fn [paths-with-elephant]
+             (map (fn [paths]
+                    (->> paths
+                      (map #(max-release (rest %) shortest-paths valves-with-flow 26))
+                      (apply max)))
+               paths-with-elephant)))
+      (map (fn [[r1 r2]] (+ r1 r2)))
+      (apply max))))
 
-;(count branches)
-;valves-with-flow
-;shortest-paths
-
-
-
-#_(println (max-release permutation shortest-paths valves-with-flow))
-
-(comment
-  lines
-
-  flows
-  valves
-  valves-with-flow
-
-
-  graph
-  (get valves-with-flow "JJ")
-  (keys graph)
-  (get (u/dijkstra graph "DD" "JJ") "JJ")
-  (count (combo/permutations (keys valves-with-flow)))
-
-
-  (sort-by val valves-with-flow)
-
-  permutation
-
-  (get-next-best-order shortest-paths valves-with-flow)
-
-  '("JJ" "DD" "HH" "BB" "EE" "CC")
-  (def best '("DD" "BB" "JJ" "HH" "EE" "CC"))
-  (max-release '("JJ" "DD" "HH" "BB" "EE" "CC") shortest-paths valves-with-flow)
-  (max-release best shortest-paths valves-with-flow)
-
-  )
+(let [lines (line-chars input)
+      valves (map #(get % 1) lines)
+      flows (flow-rates input)
+      shortest-paths (->> lines
+                       tunnel-graph
+                       find-shortest-paths)
+      valves-with-flow (valves->flow valves flows)]
+  (assert (= (part1 shortest-paths valves-with-flow) 2056))
+  (assert (= (part2 shortest-paths valves-with-flow) 2513)))
